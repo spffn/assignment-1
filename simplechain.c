8203afa76b1c91d4d0907015db6a9f9f6f7a6d9e
spffn <tc5v8@mail.umsl.edu>
2017-09-07 22:41:40 -0500

    Fixing GetOpt, Other Edits
    - having issue where even without an argument for -n, the program will
    display at least one process (the process for the program itself?)
    - shortened the output again so it's easier to read and takes up less
    space

--- simplechain.c
+++ simplechain.c
@@ -8,37 +8,47 @@ int main (int argc, char *argv[]) {
 	pid_t childpid = 0;
 	char errstr[50];
 	snprintf(errstr, sizeof errstr, "%s: Error: ", argv[0]);
-	int i, n;
-	opterr = 3;		// minimum of three arguments necessary
-					// program name, # of processes to spawn and -h flag
-	
+	int i, n = 0, c;
+	opterr = 0;	
 
-	if (argc > 3 || argc < 2) { /* check for valid number of command-line arguments */
-		perror(errstr);
-		fprintf(stderr, "Invalid number of arguments.");
-		return 1;
-	}
-	else {
-		while ((c = getopt(argc, argv, "h")) != -1) {
-			switch (c) {
-				case 'h':
-					fprintf(stderr, "REQUIRED ARGUMENTS: \n");
-					fprintf(stderr, "This program requires a command line argument for number of processes to spawn.\n");
-					fprintf(stderr, "After ./a.out, please include a number.\n");
-					break;
-				case '?':
+	// parse the command line arguments
+	while ((c = getopt(argc, argv, "n:h")) != -1) {
+		switch (c) {
+			// sets the number of processes to create, requires argument
+			case 'n':
+				n = atoi(optarg);
+				break;
+			// show help
+			case 'h':
+				fprintf(stderr, "HELP LIST: \n");
+				fprintf(stderr, "-n: \n ");
+				fprintf(stderr, "Sets the number of child processes to spawn. \n");
+				fprintf(stderr, "Must be a number. \n");
+				fprintf(stderr, "-h: \n ");
+				fprintf(stderr, "Show help, valid options and required arguments. \n\n ");
+				break;
+			case '?':
+				if(optopt == 'n'){
+					perror(errstr);
+					fprintf(stderr, "-n requires an argument. \n", optopt);
+				}
+				else if(isprint(optopt)){
+					perror(errstr);
+					fprintf(stderr, "Invalid option(s): %s \n", optopt);
+				}
+				else {
 					perror(errstr);
 					fprintf(stderr, "Invalid argument(s): %s \n", optopt);
-					return 1;
-				default:
-					perror(errstr);
-					fprintf(stderr, "Invalid argument(s): %s \n", optopt);
-					return 1;
-			}
+				}
+				return 1;
+			default:
+				perror(errstr);
+				fprintf(stderr, "Invalid argument(s): %s \n", optopt);
+				return 1;
 		}
 	}
-	
-	n = atoi(argv[1]);
+
+	// fork off the # of times given by the user
 	for (i = 1; i < n; i++) {
 		if (childpid = fork()) {
 			break;
@@ -47,6 +57,6 @@ int main (int argc, char *argv[]) {
 	
 	// Print the id of the current process before it terminates, as well as its parents id.
 	// sleep(10);
-	fprintf(stderr, "\n #%d \n Process ID: %ld \n Parent ID: %ld \n Child ID: %ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
+	fprintf(stderr, "#%d | Process ID: %ld | Parent ID: %ld | Child ID: %ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
 	return 0;
 }
