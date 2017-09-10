067b93284594c7c874bec0b32f28a9fcd900ab6a
spffn <tc5v8@mail.umsl.edu>
2017-09-10 13:20:31 -0500

    Fixing GetOpt
    - added more comments
    - included the case for missing arguments
    - more rearranging for clarity when printing

--- simplechain.c
+++ simplechain.c
@@ -1,62 +1,74 @@
+/* Taylor Clark
+CS 4760
+Assignment #1
+Unix System Calls and Library Functions */
+
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// This program takes in an argument from the command line and creates that many child processes.
+/* This program takes in an argument from the command line and creates that many child processes. It also displays a list of allowed command line arguments
+when the -h flag is used. */
 int main (int argc, char *argv[]) {
 
 	pid_t childpid = 0;
 	char errstr[50];
 	snprintf(errstr, sizeof errstr, "%s: Error: ", argv[0]);
-	int i, n = 0, c;
+	int i, n, c;
 	opterr = 0;	
 
 	// parse the command line arguments
-	while ((c = getopt(argc, argv, "n:h")) != -1) {
+	while ((c = getopt(argc, argv, ":n:h")) != -1) {
 		switch (c) {
-			// sets the number of processes to create, requires argument
+			// sets the number of processes to create
+			// -n requires an argument to work
 			case 'n':
 				n = atoi(optarg);
 				break;
 			// show help
 			case 'h':
+				fprintf(stderr, "\n----------\n");
 				fprintf(stderr, "HELP LIST: \n");
-				fprintf(stderr, "-n: \n ");
-				fprintf(stderr, "Sets the number of child processes to spawn. \n");
-				fprintf(stderr, "Must be a number. \n");
-				fprintf(stderr, "-h: \n ");
-				fprintf(stderr, "Show help, valid options and required arguments. \n\n ");
+				fprintf(stderr, "-n: \n");
+				fprintf(stderr, "\tSets the number of child processes to spawn. \n");
+				fprintf(stderr, "\tMust be a number. \n");
+				fprintf(stderr, "\tex: -n 5 \n");
+				fprintf(stderr, "-h: \n");
+				fprintf(stderr, "\tShow help, valid options and required arguments. \n");
+				fprintf(stderr, "----------\n\n");
 				break;
-			case '?':
-				if(optopt == 'n'){
-					perror(errstr);
-					fprintf(stderr, "-n requires an argument. \n", optopt);
-				}
-				else if(isprint(optopt)){
-					perror(errstr);
-					fprintf(stderr, "Invalid option(s): %s \n", optopt);
-				}
-				else {
-					perror(errstr);
-					fprintf(stderr, "Invalid argument(s): %s \n", optopt);
-				}
-				return 1;
-			default:
+			// if no argument is given for n, print an error and end.
+			case ':':
 				perror(errstr);
-				fprintf(stderr, "Invalid argument(s): %s \n", optopt);
+				fprintf(stderr, "-n requires an argument. \n", optopt);
+				return 1;
+			// if an invalid option is caught, print that it is invalid and end
+			case '?':
+				perror(errstr);
+				fprintf(stderr, "Invalid option(s): %s \n", optopt);
 				return 1;
 		}
 	}
 
-	// fork off the # of times given by the user
-	for (i = 1; i < n; i++) {
-		if (childpid = fork()) {
-			break;
+	// fork off the # of times given by the user, so long as n is
+	// greater than 0
+	if(n > 0){
+		for (i = 1; i < n; i++) {
+			if (childpid = fork()) {
+				break;
+			}
 		}
 	}
+	else { 
+		// n was never given and we should end early to prevent a print out
+		// of the current process info
+		return 1;
+	}
+	
+	// sleep(10);
 	
 	// Print the id of the current process before it terminates, as well as its parents id.
-	// sleep(10);
 	fprintf(stderr, "#%d | Process ID: %ld | Parent ID: %ld | Child ID: %ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
 	return 0;
 }
